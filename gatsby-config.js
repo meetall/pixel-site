a120b21ee4175a576f0ff4c50c2f89922c10b9cb
meetall <wgd0414@gmail.com>
2023-08-16 15:06:26 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
       resolve: `gatsby-plugin-gdpr-cookies`,
       options: {
         facebookPixel: {
-          pixelId: "979318013005156",
+          pixelId: "23702230319737",
           cookieName: "cookie-consent",
         },
         environments: ["production", "development"],
